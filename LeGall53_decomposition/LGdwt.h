489bbd749f6ff7ed029d88f9557f3369ee73f4a6
V.V.T. <vasilich.tregub@gmail.com>
2024-12-01 16:41:10 +0700

    Le Gall 5/3 decomposition in C++

--- LeGall53_decomposition/LGdwt.h
+++ LeGall53_decomposition/LGdwt.h
@@ -1,12 +1,14 @@
 #pragma once
-void dwt_inverse(std::vector<int32_t>& im, const int level)
+
+typedef void (*filter_func_t)(std::vector<int32_t>& im, const int start, const int level); // start pixel in either col (vert) or row (hor)
+
+void dwt_inverse_filter(std::vector<int32_t>& im, const int current, const int inc)
 {
-	const int inc = (int)1 << level;
 	int end = (int)im.size();
 	assert(inc < end && "stepping outside source image");
 
 	// low pass filter, {-1./4, 1./4, -1./4}
-	int i = 0;
+	int i = current;
 	im[i] -= (im[inc] + 1) >> 1;
 	i += 2 * inc;
 	for (; i < end - inc; i += 2 * inc)
@@ -22,7 +24,7 @@ void dwt_inverse(std::vector<int32_t>& im, const int level)
 	// successive convolutions with {-1./4, 1./4, -1./4} for even pixels
 	// and {1./2, 1., 1./2} for even pixels
 	// for im[n] result in -im[n-2]/8 + im[n-1]/8 + 6*im[n]/8 + im[n+1]/8 - im[n+2]/8
-	i = inc;
+	i = current + inc;
 	for (; i < end - inc; i += 2 * inc)
 	{
 		im[i] += (im[i - inc] + im[i + inc]) >> 1;
@@ -34,13 +36,12 @@ void dwt_inverse(std::vector<int32_t>& im, const int level)
 
 }
 
-void dwt_forward(std::vector<int32_t>& im, const int level)
+void dwt_forward_filter(std::vector<int32_t>& im, const int current, const int inc)
 {
-	const int inc = (int)1 << level;
 	int end = (int)im.size();
 	assert(inc < end && "stepping outside source image");
 
-	int i = inc;
+	int i = current + inc;
 	// high pass filter, {-1./2, 1., -1./2}
 	for (; i < end - inc; i += 2 * inc)
 	{
@@ -51,7 +52,7 @@ void dwt_forward(std::vector<int32_t>& im, const int level)
 		im[i] -= im[i - inc];
 	}
 
-	i = 0;
+	i = current;
 	// low pass filter, 
 	// successive convolutions with {-1./2, 1., -1./2} for odd pixels
 	// and {1./4, 1., 1./4} for even pixels
@@ -67,4 +68,47 @@ void dwt_forward(std::vector<int32_t>& im, const int level)
 	{
 		im[i] += (im[i - inc] + 1) >> 1;
 	}
+}
+
+void dwt_transform_vertical(std::vector<int32_t>& im, int width, int height, const int h_level, const int v_level, filter_func_t filter)
+{
+	assert(h_level >= 0 && v_level >= 0);
+	const int x_inc = 1 << h_level;
+	const int y_inc = width << v_level;
+	for (int ix = 0; ix < width; ix += x_inc)
+	{
+		filter(im, ix, y_inc);
+	}
+}
+
+void dwt_transform_horizontal(std::vector<int32_t>& im, int width, int height, const int h_level, const int v_level, filter_func_t filter)
+{
+	assert(h_level >= 0 && v_level >= 0);
+	const int x_inc = 1 << h_level;
+	const int y_inc = width << v_level;
+	for (int iy = 0; iy < height; iy += y_inc)
+	{
+		filter(im, iy * width, x_inc);
+	}
+}
+
+void dwt_forward_transform(std::vector<int32_t>& im, int width, int height, const int h_level, const int v_level)
+{
+	assert(im.size() == width * height);
+	assert(v_level <= h_level);
+	int d = 0;
+	for (; d < v_level; ++d)
+	{
+		dwt_transform_vertical(im, width, height, d, d, dwt_forward_filter);
+		dwt_transform_horizontal(im, width, height, d, d, dwt_forward_filter);
+	}
+	for (; d < h_level; ++d)
+	{
+		dwt_transform_horizontal(im, width, height, d, d, dwt_forward_filter);
+	}
+}
+
+void dwt_inverse_transform()
+{
+
 }
--- LeGall53_decomposition/LeGall53_decomposition.cpp
+++ LeGall53_decomposition/LeGall53_decomposition.cpp
@@ -9,6 +9,7 @@ extern "C" {
 #include <assert.h>
 #include "image.h"
 #include "LGdwt.h"
+#include "png_readwrite.h"
 
 int main()
 {
@@ -18,11 +19,41 @@ int main()
     im.depth = -1;
     im.comps_array[2] = im.comps_array[1] = im.comps_array[0] = NULL;
     ppm_decode("../19.jxs.ppm", &im);
+    int h_level = 5;
+    int v_level = 2;
+
     std::vector<int32_t> im0(&(im.comps_array[0][0]), &(im.comps_array[0][im.width * im.height]));
     std::vector<int32_t> im1(&(im.comps_array[1][0]), &(im.comps_array[1][im.width * im.height]));
     std::vector<int32_t> im2(&(im.comps_array[2][0]), &(im.comps_array[2][im.width * im.height]));
-    dwt_forward(im0, 0);
-    dwt_forward(im1, 0);
-    dwt_forward(im2, 0);
+    // upscale image data to 20 bit precision
+    uint8_t Bw = 20;
+    const uint8_t s = Bw - (uint8_t)im.depth;
+    const int32_t dclev = ((1 << Bw) >> 1); // DC level
+    for (int i = 0; i < im.width * im.height; ++i)
+    {
+        im0[i] = im0[i] << s;
+        im1[i] = im1[i] << s;
+        im2[i] = im2[i] << s;
+    }
+    dwt_forward_transform(im0, im.width, im.height, h_level, v_level);
+    dwt_forward_transform(im1, im.width, im.height, h_level, v_level);
+    dwt_forward_transform(im2, im.width, im.height, h_level, v_level);
+    for (int i = 0; i < im.width * im.height; ++i)
+    {
+        im0[i] = im0[i] >> s;
+        im1[i] = im1[i] >> s;
+        im2[i] = im2[i] >> s;
+    }
+    std::vector<uint32_t> buf(im.width * im.height);
+    for (int i = 0; i < im.width * im.height; ++i)
+    {
+        int rs = (1 << im.depth) / 256; // right shift, 12 bit positions
+        buf[i] =
+            (uint8_t)(im2[i] / rs) * 256 * 256 + // red
+            (uint8_t)(im1[i] / rs) * 256 + // green
+            (uint8_t)(im0[i] / rs); // blue
+    }
+    if (png_write(im.width, im.height, buf, L"decomposition_after_inline_DWT.png"))
+        printf("cannot write image file 'decomposition_after_inline_DWT.png'\n");
     return 0;
 }
