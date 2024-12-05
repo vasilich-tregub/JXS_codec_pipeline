bd2762220c5ec10d279bc7072d72a3832615bb2d
V.V.T. <vasilich.tregub@gmail.com>
2024-12-05 13:03:19 +0700

    minor tweaks in comments

--- LeGall53_decomposition(double_t)/LGdwt(double_t).h
+++ LeGall53_decomposition(double_t)/LGdwt(double_t).h
@@ -23,7 +23,7 @@ void dwt_inverse_filter(std::vector<double>& im, const int current, const int in
 	// high pass filter, {-1./8, 1./8, 6./8, 1./8 -1./8}
 	// successive convolutions with {-1./4, 1./4, -1./4} for even pixels
 	// and {1./2, 1., 1./2} for even pixels
-	// for im[n] result in -im[n-2]/8 + im[n-1]/8 + 6*im[n]/8 + im[n+1]/8 - im[n+2]/8
+	// for im[n] result is -im[n-2]/8 + im[n-1]/8 + 6*im[n]/8 + im[n+1]/8 - im[n+2]/8
 	i = current + inc;
 	for (; i < end - inc; i += 2 * inc)
 	{
@@ -56,7 +56,7 @@ void dwt_forward_filter(std::vector<double>& im, const int current, const int in
 	// low pass filter, 
 	// successive convolutions with {-1./2, 1., -1./2} for odd pixels
 	// and {1./4, 1., 1./4} for even pixels
-	// for im[n] result in -im[n-2]/8 + im[n-1]/4 + 6*im[n]/8 + im[n+1]/4 - im[n+2]/8
+	// for im[n] result is -im[n-2]/8 + im[n-1]/4 + 6*im[n]/8 + im[n+1]/4 - im[n+2]/8
 	// i.e., {-1./8, 2./8, 6./8, 2./8, -1./8}
 	im[i] += (im[inc] + 1) * 2;
 	i += 2 * inc;
