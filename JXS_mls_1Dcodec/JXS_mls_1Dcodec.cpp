25ac56f20b8d1256375993c2ccda57d947dd1e21
V.V.T. <vasilich.tregub@gmail.com>
2024-12-30 12:29:58 +0700

    precinct_bufs mimic ref software's precinct[col]->sif_mag_data_mb with the exercise's implied xs_config data

--- JXS_mls_1Dcodec/JXS_mls_1Dcodec.cpp
+++ JXS_mls_1Dcodec/JXS_mls_1Dcodec.cpp
@@ -5,9 +5,22 @@
 #include <vector>
 #include "LGT.h"
 
+int32_t clamp(int32_t v, int32_t max_v)
+{
+	if (v > max_v)
+	{
+		return max_v;
+	}
+	if (v < 0)
+	{
+		return 0;
+	}
+	return v;
+}
+
 int main()
 {
-	uint8_t NLx = 3; // Number of levels in x (decomp_h)
+	uint8_t NLx = 5; // Number of levels in x (decomp_h)
 	if (NLx > 5)
 	{
 		std::cout << "NLx=" << (uint32_t)NLx << " is greater than max allowed decomp level (5)\n";
@@ -38,23 +51,31 @@ int main()
 	}                                            // the last array has the earliest details, array of size of half source data length
 	precinct_bufs[0].resize(bufsize);    // precinct->sig_mag_data_mb->bufs[sig_mag_data_mb->n_buffers-1] is given by dwt_forward(im, 0)
 
-	int d = 1;
+	int d = 1; // isolate value sign into msb
 	for (int lvl = 1; lvl <= NLx; ++lvl)
 	{
 		d = 1 << lvl;
 		for (int ix = 0; ix < width / d; ++ix)
 		{
-			precinct_bufs[NLx + 1 - lvl][ix] = im[d * ix + (1 << (lvl - 1))]; // horizontal detail coeffs
+			int32_t val = im[d * ix + (1 << (lvl - 1))]; // horizontal detail coeff
+			if (val >= 0)
+				precinct_bufs[NLx + 1 - lvl][ix] = val;
+			else
+				precinct_bufs[NLx + 1 - lvl][ix] = -val + (1 << 31)/*SIGN_BIT_MASK*/;
+
 		}
 	}
 	for (int ix = 0; ix < width / d; ++ix)
 	{
-		precinct_bufs[0][ix] = im[d * ix]; // approximation coeffs
+		int32_t val = im[d * ix]; // approximation coeff
+		if (val >= 0)
+			precinct_bufs[0][ix] = val;
+		else
+			precinct_bufs[0][ix] = -val + (1 << 31)/*SIGN_BIT_MASK*/;
 	}
 
 	for (int i = 0; i < width; ++i)
 	{
-
 		std::cout << im[i] << " ";
 	}
 	std::cout << "\n";
@@ -64,6 +85,13 @@ int main()
 	{
 		dwt_inverse(im, i);
 	}
+	/*	const uint8_t s = Bw - (uint8_t)im->depth;
+	const xs_data_in_t dclev_and_rounding = ((1 << Bw) >> 1) + ((1 << s) >> 1);
+	Bw = depth as Fq (fractional bits) is zero in this exercise*/
+	int32_t dclev_and_rounding = ((1 << depth) >> 1);
+	int32_t max_val = (1 << depth) - 1;
+	for (int i = 0; i < width; ++i)
+		im[i] = clamp((im[i] + dclev_and_rounding), max_val);
 	for (int i = 0; i < width; ++i)
 		std::cout << im[i] << " ";
 	std::cout << "\n";
